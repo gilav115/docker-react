e2cf4aac2fc532b66c9204232d8183db3404c56b
Gil Avraham <gavraham@apple.coml>
2020-04-29 12:25:16 +0300

    some change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          this is the first one
         </a>
       </header>
     </div>
